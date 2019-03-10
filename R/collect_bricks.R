bd4cc5df4005d9fd1e12e1841662ba73c0b7f4da
Ryan Timpe <ryan.timpe@gmail.com>
2019-03-10 11:30:18 -0400

    Moved collect_bricks() to a new script as it's used in both Mosaics and 3D Models

--- R/collect_bricks.R
+++ R/collect_bricks.R
@@ -1,65 +1,3 @@
-#' Helper function to match an RGB color to a subset of allowed colors
-#'
-#' @param R,G,B R, G, and B color values between 0 and 1.
-#' @param dat_color Data frame of allowed color. Use the attached data \code{lego_colors.rda}.
-#' @return A data frame of a matched color with closest Euclidean distance to the input R, G, & B.
-#' @export 
-#' 
-convert_to_match_color <- function(R, G, B, dat_color){
-  dat_color %>% 
-    dplyr::mutate(dist = ((R_lego - R)^2 + (G_lego - G)^2 + (B_lego - B)^2)^(1/2)) %>% 
-    dplyr::top_n(-1, dist) %>% 
-    dplyr::mutate(Lego_color = grDevices::rgb(R_lego, G_lego, B_lego)) %>% 
-    dplyr::select(Lego_name = Color, Lego_color)
-}
-
-#' Convert image output from scale_image() to bricks
-#'
-#' @param image_list List output from scale_image(). Contains an element  \code{Img_scaled}.
-#' @param color_table Defaults to \code{lego_colors}. Data frame of brick colors to map onto image. Must contain Name and R, G, B channels. See attached data  \code{lego_colors} as examples.
-#' @param theme Theme of brick colors to use. Set to \code{"bw"} for grayscale mosaics.
-#' @param contrast For \code{theme = "bw"}. A value >1 will increase the contrast of the image while a positive value <1 will decrease the contrast.
-#' @return A list with element \code{Img_lego} containing a data frame of the x- & y-coordinates, R, G, B channels, and mapped color of each brick (pixel).
-#' @export 
-#'
-legoize <- function(image_list, color_table = lego_colors, theme = "default", contrast = 1){
-  in_list <- image_list
-  
-  if(theme == "default"){
-    #Speed up calc by round pixel to nearest 1/20 & only calculating unique
-    mosaic_colors <- in_list$Img_scaled %>% 
-      dplyr::mutate_at(dplyr::vars(R, G, B), dplyr::funs(round(.*20)/20)) %>% 
-      dplyr::select(R, G, B) %>% 
-      dplyr::distinct() %>% 
-      dplyr::mutate(lego = purrr::pmap(list(R, G, B), convert_to_match_color, color_table)) %>% 
-      tidyr::unnest(lego)
-    
-    img <- in_list$Img_scaled %>% 
-      dplyr::mutate_at(dplyr::vars(R, G, B), dplyr::funs(round(.*20)/20)) %>%
-      dplyr::left_join(mosaic_colors, by = c("R", "G", "B"))
-    
-  } else if (theme == "bw"){
-    #Black and white is simpler... cut the colors into 4 groups, then assign lightest = white, darkest = black
-    bw_colors <- color_table %>% 
-      dplyr::filter(t_BW) %>% 
-      dplyr::arrange((R_lego + G_lego + B_lego)) %>% 
-      dplyr::mutate(Lego_color = grDevices::rgb(R_lego, G_lego, B_lego))
-    
-    img <- in_list$Img_scaled %>% 
-      dplyr::mutate(shade = (R+G+B)/3,
-                    shade = shade ^ contrast) %>% 
-      dplyr::mutate(shade_bw = as.numeric(as.factor(cut(shade, 4)))) %>% 
-      dplyr::mutate(Lego_name = bw_colors$Color[shade_bw],
-                    Lego_color = bw_colors$Lego_color[shade_bw]) %>% 
-      dplyr::select(-dplyr::starts_with("shade"))
-    
-  }
-  in_list[["Img_lego"]] <- img %>% 
-    dplyr::mutate(Level = 1)
-  
-  return(in_list)
-  
-}
 
 #' Collect legoize image from individual bricks into grouped bricks.
 #'
@@ -77,39 +15,39 @@ collect_bricks <- function(image_list, mosaic_type = "flat"){
       #4x2 bricks - horizontal
       dplyr::group_by(Level, xg = x %/% 4, yg = y %/% 2) %>% 
       dplyr::mutate(g_1_x4y2_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 8,
-                                 paste0("x4y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x4y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #4x2 bricks - vertical
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x %/% 2, yg = y %/% 4) %>% 
       dplyr::mutate(g_2_x2y4_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 8,
-                                 paste0("x2y4_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x2y4_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #2x2 bricks
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x %/% 2, yg = y %/% 2) %>% 
       dplyr::mutate(g_5_x2y2_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 4,
-                                 paste0("x2y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x2y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #4x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x %/% 4, yg = y ) %>% 
       dplyr::mutate(g_7_x4y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 4,
-                                 paste0("x4y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x4y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #4x1 bricks -  vertical
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x, yg = y %/% 4) %>% 
       dplyr::mutate(g_8_x1y4_1 = ifelse(length(unique(Lego_name)) == 1 & n() == 4,
-                                 paste0("x1y4_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x1y4_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #3x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x %/% 3, yg = y ) %>% 
       dplyr::mutate(g_7_x3y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 3,
-                                 paste0("x3y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x3y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #3x1 bricks -  vertical
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x, yg = y %/% 3) %>% 
       dplyr::mutate(g_8_x1y3_1 = ifelse(length(unique(Lego_name)) == 1 & n() == 3,
-                                 paste0("x1y3_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x1y3_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #2x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x %/% 2, yg = y ) %>% 
       dplyr::mutate(g_9_x2y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 2,
-                                 paste0("x2y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x2y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #2x1 bricks -  vertical
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = x, yg = y %/% 2) %>% 
       dplyr::mutate(g_a_x1y2_1 = ifelse(length(unique(Lego_name)) == 1 & n() == 2,
-                                 paste0("x1y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x1y2_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       dplyr::ungroup() %>% 
       #1x1
       dplyr::mutate(g_b_x1y1_0 = paste0("x1y1_", "x", x, "_y", y, "_", Level)) %>% 
@@ -121,15 +59,15 @@ collect_bricks <- function(image_list, mosaic_type = "flat"){
       #4x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = (x + y %% 4) %/% 4, yg = y ) %>% 
       dplyr::mutate(g_7_x4y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 4,
-                                 paste0("x4y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x4y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #3x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = (x + y %% 3) %/% 3, yg = y ) %>% 
       dplyr::mutate(g_7_x3y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 3,
-                                 paste0("x3y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x3y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       #2x1 bricks - horizontal
       dplyr::ungroup() %>% dplyr::group_by(Level, xg = (x + y %% 2) %/% 2, yg = y ) %>% 
       dplyr::mutate(g_9_x2y1_0 = ifelse(length(unique(Lego_name)) == 1 & n() == 2,
-                                 paste0("x2y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
+                                        paste0("x2y1_", "x", min(x), "_y", min(y), "_", Level), NA)) %>% 
       dplyr::ungroup() %>% 
       #1x1
       dplyr::mutate(g_b_x1y1_0 = paste0("x1y1_", "x", x, "_y", y, "_", Level)) %>% 
@@ -173,7 +111,7 @@ collect_bricks <- function(image_list, mosaic_type = "flat"){
     dplyr::summarise(xmin = min(x)-0.5, xmax = max(x)+0.5,
                      ymin = min(y)-0.5, ymax = max(y)+0.5) %>% 
     dplyr::ungroup()
-
+  
   # This is very brute-force. Probably a much cleaner way to do this
   pcs <- img2 %>% 
     dplyr::select(Level, Brick, brick_id, Lego_name, Lego_color) %>% 
---
+++ R/image_to_colors.R
@@ -0,0 +1,62 @@
+#' Helper function to match an RGB color to a subset of allowed colors
+#'
+#' @param R,G,B R, G, and B color values between 0 and 1.
+#' @param dat_color Data frame of allowed color. Use the attached data \code{lego_colors.rda}.
+#' @return A data frame of a matched color with closest Euclidean distance to the input R, G, & B.
+#' @export 
+#' 
+convert_to_match_color <- function(R, G, B, dat_color){
+  dat_color %>% 
+    dplyr::mutate(dist = ((R_lego - R)^2 + (G_lego - G)^2 + (B_lego - B)^2)^(1/2)) %>% 
+    dplyr::top_n(-1, dist) %>% 
+    dplyr::mutate(Lego_color = grDevices::rgb(R_lego, G_lego, B_lego)) %>% 
+    dplyr::select(Lego_name = Color, Lego_color)
+}
+
+#' Convert image output from scale_image() to bricks
+#'
+#' @param image_list List output from scale_image(). Contains an element  \code{Img_scaled}.
+#' @param color_table Defaults to \code{lego_colors}. Data frame of brick colors to map onto image. Must contain Name and R, G, B channels. See attached data  \code{lego_colors} as examples.
+#' @param theme Theme of brick colors to use. Set to \code{"bw"} for grayscale mosaics.
+#' @param contrast For \code{theme = "bw"}. A value >1 will increase the contrast of the image while a positive value <1 will decrease the contrast.
+#' @return A list with element \code{Img_lego} containing a data frame of the x- & y-coordinates, R, G, B channels, and mapped color of each brick (pixel).
+#' @export 
+#'
+legoize <- function(image_list, color_table = lego_colors, theme = "default", contrast = 1){
+  in_list <- image_list
+  
+  if(theme == "default"){
+    #Speed up calc by round pixel to nearest 1/20 & only calculating unique
+    mosaic_colors <- in_list$Img_scaled %>% 
+      dplyr::mutate_at(dplyr::vars(R, G, B), dplyr::funs(round(.*20)/20)) %>% 
+      dplyr::select(R, G, B) %>% 
+      dplyr::distinct() %>% 
+      dplyr::mutate(lego = purrr::pmap(list(R, G, B), convert_to_match_color, color_table)) %>% 
+      tidyr::unnest(lego)
+    
+    img <- in_list$Img_scaled %>% 
+      dplyr::mutate_at(dplyr::vars(R, G, B), dplyr::funs(round(.*20)/20)) %>%
+      dplyr::left_join(mosaic_colors, by = c("R", "G", "B"))
+    
+  } else if (theme == "bw"){
+    #Black and white is simpler... cut the colors into 4 groups, then assign lightest = white, darkest = black
+    bw_colors <- color_table %>% 
+      dplyr::filter(t_BW) %>% 
+      dplyr::arrange((R_lego + G_lego + B_lego)) %>% 
+      dplyr::mutate(Lego_color = grDevices::rgb(R_lego, G_lego, B_lego))
+    
+    img <- in_list$Img_scaled %>% 
+      dplyr::mutate(shade = (R+G+B)/3,
+                    shade = shade ^ contrast) %>% 
+      dplyr::mutate(shade_bw = as.numeric(as.factor(cut(shade, 4)))) %>% 
+      dplyr::mutate(Lego_name = bw_colors$Color[shade_bw],
+                    Lego_color = bw_colors$Lego_color[shade_bw]) %>% 
+      dplyr::select(-dplyr::starts_with("shade"))
+    
+  }
+  in_list[["Img_lego"]] <- img %>% 
+    dplyr::mutate(Level = 1)
+  
+  return(in_list)
+  
+}
